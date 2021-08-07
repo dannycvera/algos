41923b2cbafe25ba0f905352b1055f64de0a1473
danv <dannyvera@gmail.com>
2021-08-07 15:33:06 -0400

    JS Gym - node anagrams notes

--- algo-meetup/JSGYM-Anagrams.js
+++ algo-meetup/JSGYM-Anagrams.js
@@ -1,26 +1,23 @@
 // detect two anagrams
-// import { argv } from "process";
-// let word1 = argv[2];
-// let word2 = argv[3];
 
 function anagram(word1, word2) {
+  // use two hash maps to store the frequecies of the letters in each word
   const hash1 = {}; //{ "a": 2, "t":1}
   const hash2 = {};
   let current;
   for (let i = 0; i < word1.length; i++) {
+    // populate the first hash map with letter frequencies from the first word
     current = word1[i].toLowerCase();
-    if (hash1.hasOwnProperty(current)) {
-      hash1[i]++;
-    } else {
-      hash1[current] = 1;
-    }
+    hash1[current] = hash1.hasOwnProperty(current)
+      ? hash1[i]++
+      : (hash1[current] = 1);
+    // populate the second hash map with letter frequencies from the second word
     current = word2[i].toLowerCase();
-    if (hash2.hasOwnProperty(current)) {
-      hash2[i]++;
-    } else {
-      hash2[current] = 1;
-    }
+    hash2[current] = hash2.hasOwnProperty(current)
+      ? hash2[i]++
+      : (hash2[current] = 1);
   }
+  // compare frequencies of each word by comparing the number values in each hash map
   for (let i = 0; i < word2.length; i++) {
     current = word2[i].toLowerCase();
     if (!hash1.hasOwnProperty(current) && hash1[current] !== hash2[current]) {
@@ -32,6 +29,8 @@ function anagram(word1, word2) {
 
 let word_1 = process.argv[2];
 let word_2 = process.argv[3];
+
+// for testing
 // let word_1 = "Tar";
 // let word_2 = "Rat";
 console.log(anagram(word_1, word_2));
